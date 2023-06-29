fe16cef5af9095ba126ff95b09199bfab71b9a06
aname <132615033+aname8364@users.noreply.github.com>
2023-06-29 23:30:07 +0900

    230629
    1

---
+++ acm/Tree/9372.py
@@ -0,0 +1,9 @@
+test_case_count = int(input())
+
+for i in range(test_case_count):
+    n, m = map(int, input().split())
+    for j in range(m):
+        a, b = map(int, input().split())
+    print(n-1)
+    
+"pypy3"
