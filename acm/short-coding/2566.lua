e4220c241a92b8b3652f9662126c9e71ec5533af
aname <132615033+aname8364@users.noreply.github.com>
2023-10-01 07:17:02 +0900

    96b to 93b

--- acm/short-coding/2566.lua
+++ acm/short-coding/2566.lua
@@ -1,1 +1,1 @@
-a=-1;for i=0,80 do x=io.read"n"if x>a then a,b,c=x,i//9+1,i%9+1;end;end;p=print;p(a)p(b..' '..c)
+a=-1;for i=0,80 do x=io.read"n"if x>a then a,b=x,i;end;end;p=print;p(a)p(b//9+1 ..' '..b%9+1)
